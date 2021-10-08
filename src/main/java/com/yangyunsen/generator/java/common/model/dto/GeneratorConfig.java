c99e6a821f127bd1ca62da5c11da177d5f53d13f
CloudS3n <www.402645063@gmail.com>
2021-10-08 11:50:45 +0800

    refactor(common-dto): 全局配置添加open api文档开关

--- src/main/java/com/yangyunsen/generator/java/common/model/dto/GeneratorConfig.java
+++ src/main/java/com/yangyunsen/generator/java/common/model/dto/GeneratorConfig.java
@@ -20,9 +20,28 @@ import java.util.Set;
 @AllArgsConstructor
 public class GeneratorConfig {
 
+    /**
+     * 代码作者
+     */
     private String author;
+    /**
+     * 生成器模式，JAP OR MP
+     */
     private Mode mode;
+    /**
+     * 包信息
+     */
     private PackageInfo packageInfo;
+    /**
+     * 数据库连接信息
+     */
     private DatabaseInfo databaseInfo;
+    /**
+     * 需要生成的表名列表
+     */
     private Set<String> tableNames;
+    /**
+     * 是否开启open api 3文档注解
+     */
+    private Boolean enableOpenApi3;
 }
